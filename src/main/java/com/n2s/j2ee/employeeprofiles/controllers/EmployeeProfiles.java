329d7f9df9297c2d40aaa12a36dc87611f0cf045
Subbyz <subby.selvaraj@gmail.com>
2018-07-15 16:36:25 -0400

    Controller created

---
+++ src/main/java/com/n2s/j2ee/employeeprofiles/controllers/EmployeeProfiles.java
@@ -0,0 +1,16 @@
+package com.n2s.j2ee.employeeprofiles.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class EmployeeProfiles {
+
+    public List<String> getAllEmployees(){
+        return null;
+
+    }
+
+
+}
