2f45ea860cf795d04c0b6e18953bef83ac227201
Subbyz <subby.selvaraj@gmail.com>
2018-07-15 16:20:49 -0400

    first commit

---
+++ src/main/java/com/n2s/j2ee/employeeprofiles/EmployeeProfilesApplication.java
@@ -0,0 +1,12 @@
+package com.n2s.j2ee.employeeprofiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeProfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeProfilesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/n2s/j2ee/employeeprofiles/EmployeeProfilesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.n2s.j2ee.employeeprofiles;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeeProfilesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
